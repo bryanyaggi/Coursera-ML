29c9c5a9320159869d6469982ac15742692155b8
Bryan Yaggi <bryan.yaggi@gmail.com>
2019-09-02 15:15:44 -0700

    Completed Programming Assignment 1.

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -17,17 +17,10 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
-
-
-
-
+    theta = theta - (alpha / m) * X' * (X * theta - y);
     % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
-
 end
-
 end
