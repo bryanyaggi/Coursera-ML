1bc0afeca9f00d06af924eec99e8a52fd58d2dbf
Bryan Yaggi <bryan.yaggi@gmail.com>
2019-09-06 13:40:34 -0700

    Completed PA2.

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -17,11 +17,13 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-
-
-
-
-
+J = (1 / m) * (-y' * log(sigmoid(X * theta)) - ...
+  (1 - y)' * log(1 - sigmoid(X * theta))) + ...
+  lambda / (2 * m) * (theta(2:end, :)' * theta(2:end, :));
+grad = (1 / m) * X' * (sigmoid(X * theta) - y);
+theta_ = theta;
+theta_(1, :) = [0]; % theta_0 should not be regularized
+grad = grad + (lambda / m) * theta_; % add regularization term
 % =============================================================
 
 end
