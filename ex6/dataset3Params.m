cb5a03594e5375778ccc2ad251db0c7784a97696
Bryan Yaggi <bryan.yaggi@gmail.com>
2019-09-22 21:43:03 -0700

    Completed PA6.

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,12 +23,33 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+% Test to find best C and sigma values
+%{
+bestError = 1.0;
+cVals = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigmaVals = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+C_best = cVals(1);
+sigma_best = sigmaVals(1);
+for i = 1:length(cVals)
+  C = cVals(i);
+  for j = 1:length(sigmaVals)
+    sigma = sigmaVals(j);
+    model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+    predictions = svmPredict(model, Xval);
+    predError = mean(double(predictions ~= yval))
+    if (predError < bestError)
+      bestError = predError;
+      C_best = C
+      sigma_best = sigma
+    end
+  end
+end
+C = C_best;
+sigma = sigma_best;
+%}
 
-
-
-
-
-
+C = 1;
+sigma = 0.1;
 % =========================================================================
 
 end
